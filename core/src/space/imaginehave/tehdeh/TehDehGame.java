5a2ed0ffa59395ce21d61c80a319af9998931089
Christopher D Williams <twig@loxwood.Home>
2017-06-19 22:47:43 +0100

    bits n bobs

--- core/src/space/imaginehave/tehdeh/TehDehGame.java
+++ core/src/space/imaginehave/tehdeh/TehDehGame.java
@@ -4,30 +4,34 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import space.imaginehave.tehdeh.screen.TehDehGameScreen;
+
 public class TehDehGame extends Game {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	public SpriteBatch	batch;
+	public BitmapFont	font;
+
 	@Override
-	public void create () {
+	public void create() {
+
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+		this.setScreen(new TehDehGameScreen(this));
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+
+		super.render();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
+
 		batch.dispose();
-		img.dispose();
+		font.dispose();
 	}
 }
--- desktop/src/space/imaginehave/tehdeh/desktop/DesktopLauncher.java
+++ desktop/src/space/imaginehave/tehdeh/desktop/DesktopLauncher.java
@@ -8,6 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Teh Deh";
+		config.width = 800;
+		config.height = 600;
 		new LwjglApplication(new TehDehGame(), config);
 	}
 }
