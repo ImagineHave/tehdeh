8e08366d73e484a0e2de1cc2494e395f5aaae8c5
oyvindwoller <oyvind.woller@imagine-have.xyz>
2017-06-22 20:29:09 +0100

    Updated gui.

--- core/src/space/imaginehave/tehdeh/screen/TehDehGameScreen.java
+++ core/src/space/imaginehave/tehdeh/screen/TehDehGameScreen.java
@@ -18,6 +18,7 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -48,78 +49,28 @@ public class TehDehGameScreen implements Screen {
 		im.addProcessor(stage);
 
 		Gdx.input.setInputProcessor(im);
+		createUI();
+	}
 
-		// A skin can be loaded via JSON or defined programmatically, either is
-		// fine. Using a skin is optional but strongly
-		// recommended solely for the convenience of getting a texture, region,
-		// etc as a drawable, tinted drawable, etc.
-		skin = new Skin();
+	private void createUI() {
+				skin = new Skin(Gdx.files.internal("skin/plain-james-ui.json"));
 
-		// Generate a 1x1 white texture and store it in the skin named "white".
-		Pixmap pixmap = new Pixmap(1, 1, Format.RGBA8888);
-		pixmap.setColor(Color.WHITE);
-		pixmap.fill();
-		skin.add("white", new Texture(pixmap));
+				Table hud = new Table();
+				hud.setBounds(600, 0, 200, 600);
+				hud.setDebug(true);
+				stage.addActor(hud);
 
-		Pixmap pixmap2 = new Pixmap(1, 1, Format.RGBA8888);
-		pixmap2.setColor(Color.FOREST);
-		pixmap2.fill();
-		skin.add("wiggle", new Texture(pixmap));
+				final ImageButton button = new ImageButton(skin);
+				button.getStyle().imageUp = new Image(game.getBucket()).getDrawable();
+				hud.add(button).top().left().expand();
 
-		Rectangle bucket = new Rectangle();
-		bucket.x = 800 / 2 - 64 / 2; // center the bucket horizontally
-		bucket.y = 20; // bottom left corner of the bucket is 20 pixels above
-						// the bottom screen edge
-		bucket.width = 64;
-		bucket.height = 64;
-
-		// Store the default libgdx font under the name "default".
-		skin.add("default", new BitmapFont());
-
-		// Configure a TextButtonStyle and name it "default". Skin resources are
-		// stored by type, so this doesn't overwrite the font.
-		TextButtonStyle textButtonStyle = new TextButtonStyle();
-		textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
-		textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
-		textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
-		textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
-		textButtonStyle.font = skin.getFont("default");
-		skin.add("default", textButtonStyle);
-
-		// Create a table that fills the screen. Everything else will go inside
-		// this table.
-		Table hud = new Table();
-		hud.setBounds(600, 0, 200, 600);
-		hud.setDebug(true);
-		stage.addActor(hud);
-
-		// Create a button with the "default" TextButtonStyle. A 3rd parameter
-		// can be used to specify a name other than "default".
-		final TextButton button = new TextButton("Click me!", skin);
-		hud.add(button);
-
-		// Add a listener to the button. ChangeListener is fired when the
-		// button's checked state changes, eg when clicked,
-		// Button#setChecked() is called, via a key press, etc. If the
-		// event.cancel() is called, the checked state will be reverted.
-		// ClickListener could have been used, but would only fire when clicked.
-		// Also, canceling a ClickListener event won't
-		// revert the checked state.
-		button.addListener(new ChangeListener() {
-
-			public void changed(ChangeEvent event, Actor actor) {
-
-				System.out.println("Clicked! Is checked: " + button.isChecked());
-				button.setText("Good job!");
-				
-				game.getState().setMouseFollow(game.getBucket());
-				game.getState().setMouseCoords(game.getState().getMouseCoords());
-			}
-		});
-
-		// Add an image actor. Have to set the size, else it would be the size
-		// of the drawable (which is the 1x1 texture).
-		hud.add(new Image(skin.newDrawable("white", Color.RED))).size(64);
+				button.addListener(new ChangeListener() {
+					public void changed(ChangeEvent event, Actor actor) {
+						System.out.println("Clicked! Is checked: " + button.isChecked());
+						game.getState().setMouseFollow(game.getBucket());
+						game.getState().setMouseCoords(game.getState().getMouseCoords());
+					}
+				});
 
 	}
 
