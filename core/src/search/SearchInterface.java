6fc6169ffa431c32ec761924493d40f35f174ff8
oyvindwoller <oyvind.woller@imagine-have.xyz>
2017-06-24 14:02:41 +0100

    Committing search interface

---
+++ core/src/search/SearchInterface.java
@@ -0,0 +1,12 @@
+package search;
+
+import java.util.List;
+
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
+
+public interface SearchInterface {
+	
+	List<Cell> calculatePath(Cell startNode, Cell goalNode);
+	
+
+}
