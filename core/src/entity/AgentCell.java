d7c5352a9da51d18164c01e11c2edb6cfff5449a
oyvindwoller <oyvind.woller@imagine-have.xyz>
2017-06-24 15:22:54 +0100

    Adding some fields to Agent Cell

--- core/src/entity/AgentCell.java
+++ core/src/entity/AgentCell.java
@@ -1,5 +1,12 @@
 package entity;
 
+import com.badlogic.gdx.math.Vector3;
+
+import search.SearchInterface;
+
 public class AgentCell extends EntityCell {
+	
+	SearchInterface searchMethod;
+	Vector3 position;
 
 }
