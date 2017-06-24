13883cfb6e683f76c22354e289e9a43e1e01e732
oyvindwoller <oyvind.woller@imagine-have.xyz>
2017-06-24 13:51:58 +0100

    Using towercell

--- core/src/entity/TowerCell.java
+++ core/src/entity/TowerCell.java
@@ -1,5 +1,16 @@
 package entity;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
+
 public class TowerCell extends EntityCell {
 
+	public TowerCell(Texture towerTexture) {
+		super();
+		TextureRegion entityTextureRegion = new TextureRegion(towerTexture);
+		TiledMapTile tmt = new StaticTiledMapTile(entityTextureRegion);
+		setTile(tmt);
+	}
 }
