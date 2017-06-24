91af8cfd746bde2f91f77a162c5c8f8a2170567a
oyvindwoller <oyvind.woller@imagine-have.xyz>
2017-06-24 09:47:59 +0100

    Added potential assets. Set tilemap to use same sprite batch as the game. Changed the bucketimage

--- core/src/space/imaginehave/tehdeh/TehDehGame.java
+++ core/src/space/imaginehave/tehdeh/TehDehGame.java
@@ -5,24 +5,18 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.maps.MapLayer;
-import com.badlogic.gdx.maps.MapObject;
-import com.badlogic.gdx.maps.objects.TextureMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.objects.TiledMapTileMapObject;
 import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import com.badlogic.gdx.math.Vector3;
 
 import space.imaginehave.tehdeh.screen.GameScreenTehDeh;
-import space.imaginehave.tehdeh.state.Entity;
 import space.imaginehave.tehdeh.state.TehDehGameState;
 import space.imaginehave.tehdeh.tiledmaprenderer.OrthogonalTiledMapRendererTehDeh;
 
@@ -31,13 +25,13 @@ public class TehDehGame extends Game {
 	private SpriteBatch	batch;
 	private TehDehGameState state;
 	private BitmapFont	font;
-	private Texture bucketImage;
+	private Texture towerImage;
 	private TiledMap tiledMap;
 	private TiledMapRenderer tiledMapRenderer;
 
 	@Override
 	public void create() {
-		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+		towerImage = new Texture(Gdx.files.internal("testTower.png"));
 		state = new TehDehGameState();
 		batch = new SpriteBatch();
 		font = new BitmapFont();
@@ -77,14 +71,15 @@ public class TehDehGame extends Game {
 	}
 	
 	public Texture getBucket(){
-		return bucketImage;
+		return towerImage;
 	}
 
 	public void addEntity(Vector3 vector, Vector3 vector2, Texture texture) {
-		TiledMapTile tmt = new StaticTiledMapTile(new TextureRegion(bucketImage));
 		TiledMapTileLayer entityLayer = (TiledMapTileLayer) tiledMap.getLayers().get("tileLayer1");
 				
 		Cell cell = new Cell();
+		TextureRegion entityTextureRegion = new TextureRegion(towerImage);
+		TiledMapTile tmt = new StaticTiledMapTile(entityTextureRegion);
 		cell.setTile(tmt);
 		
 		entityLayer.setCell((int) (vector.x/entityLayer.getTileWidth()), (int) (vector.y/entityLayer.getTileHeight()), cell);
