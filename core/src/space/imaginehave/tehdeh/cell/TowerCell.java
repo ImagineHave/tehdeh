a0c384aa363df93c8005e652f8c23dcf24fa3190
cwih <chris.williams@imagine-have.xyz>
2017-07-10 19:08:52 +0100

    cell package

--- core/src/space/imaginehave/tehdeh/cell/TowerCell.java
+++ core/src/space/imaginehave/tehdeh/cell/TowerCell.java
@@ -1,4 +1,4 @@
-package space.imaginehave.tehdeh.entity;
+package space.imaginehave.tehdeh.cell;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
--- core/src/space/imaginehave/tehdeh/layer/LayerService.java
+++ core/src/space/imaginehave/tehdeh/layer/LayerService.java
@@ -8,7 +8,7 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 
 import space.imaginehave.tehdeh.Constant;
-import space.imaginehave.tehdeh.entity.TowerCell;
+import space.imaginehave.tehdeh.cell.TowerCell;
 import space.imaginehave.tehdeh.state.GameStateTehDeh;
 import space.imaginehave.tehdeh.tower.TowerMapObject;
 
