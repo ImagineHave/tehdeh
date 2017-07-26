bb1dd4a78d776b2920a2c5811018c16f48ed4a0a
oyvindwoller <oyvind.woller@imagine-have.xyz>
2017-07-26 19:59:22 +0100

    AgentProfitted on player is now called when agent is killed

--- core/src/space/imaginehave/tehdeh/daemon/DaemonMapObject.java
+++ core/src/space/imaginehave/tehdeh/daemon/DaemonMapObject.java
@@ -40,6 +40,7 @@ public class DaemonMapObject extends MapObject {
 			if(Intersector.overlapConvexPolygons(projectile.getBoundingBox(), agent.getBoundingBox())) {
 				agent.doDamage(projectile.getDamage());
 				if(agent.isDead()) {
+					state.getPlayer().agentProfitted(agent);
 					killAgent(agent);
 				}
 				state.getAgentLayer().getObjects().remove(projectile);
